00a9c10bf39f6f1ede33535c5da865101ea4a1a4
carlosejesus21-commits <carlos.e.jesus21@aluno.senai.br>
2025-10-22 16:50:09 -0300

    Add files via upload

---
+++ Portaria/script.js
@@ -0,0 +1,25 @@
+function Check(){
+    var wasBorn = parseInt(document.getElementById("birth").value)
+    var res = document.getElementById("result")
+    var clientAge = document.getElementById("age")
+
+    if(isNaN(wasBorn)){
+        res.textContent = "Type only numbers 🚫"
+        res.style.color = "red"
+        return
+    }
+    const thisYear = new Date().getFullYear()
+    const age = thisYear - wasBorn
+
+    if(age >= 18){
+        res.textContent = "Over 18, ENTRY ✅"
+        clientAge.textContent = age
+        document.getElementById("v1").style.display = "block"
+        document.getElementById("v2").style.display = "none"
+    }else{
+         res.textContent = "Under 18, NOT ALLOWED 🔞"
+         clientAge.textContent = age
+         document.getElementById("v2").style.display = "block"
+         document.getElementById("v1").style.display = "none"
+    }
+}
